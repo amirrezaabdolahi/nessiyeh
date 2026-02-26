e49079b7c4bc73d636a68a761a171b6411248db8
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-02-26 22:49:02 +0330

    started the dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,34 +9,7 @@ export default function MainLayout({
 }) {
     return (
         <div className="w-full h-screen">
-            <Card className="w-full p-4!  flex items-center justify-between">
-                <div className="flex items-center gap-4">
-                    <Typography
-                        variant="h6"
-                        className="text-center border-l border-gray-300 pl-4"
-                    >
-                        داشبورد
-                    </Typography>
-                    <ChoseMarketDialog />
-                </div>
-                <div className="flex items-center gap-4">
-                    <TextField
-                        variant="outlined"
-                        size="small"
-                        label="جستجو"
-                        placeholder="فروش ، خرید ، ..."
-                    />
-                    <IconButton size="large">
-                        <Notifications />
-                    </IconButton>
-                    <Avatar
-                        className="cursor-pointer"
-                        sx={{ bgcolor: "primary.main" }}
-                    >
-                        P
-                    </Avatar>
-                </div>
-            </Card>
+            
             {children}
         </div>
     );
---
+++ src/features/dashboard/components/DashboardNavBar.tsx
@@ -0,0 +1,38 @@
+import ChoseMarketDialog from "@/features/dashboard/components/ChoseMarketDialog";
+import { Notifications } from "@mui/icons-material";
+import { Avatar, Card, IconButton, TextField, Typography } from "@mui/material";
+
+const DashboardNavBar = () => {
+    return (
+        <Card className="w-full p-4!  flex items-center justify-between">
+            <div className="flex items-center gap-4">
+                <Typography
+                    variant="h6"
+                    className="text-center border-l border-gray-300 pl-4"
+                >
+                    داشبورد
+                </Typography>
+                <ChoseMarketDialog />
+            </div>
+            <div className="flex items-center gap-4">
+                <TextField
+                    variant="outlined"
+                    size="small"
+                    label="جستجو"
+                    placeholder="فروش ، خرید ، ..."
+                />
+                <IconButton size="large">
+                    <Notifications />
+                </IconButton>
+                <Avatar
+                    className="cursor-pointer"
+                    sx={{ bgcolor: "primary.main" }}
+                >
+                    P
+                </Avatar>
+            </div>
+        </Card>
+    );
+};
+
+export default DashboardNavBar;
