058841246fb31807bbd759a0da85c96234c5d329
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-02-28 23:06:45 +0330

    some changes

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,19 +1,20 @@
 import DashboardsPageHeader from "@/components/dash/DashboardsPageHeader";
 import { dashboardCards } from "@/data/DashboardDatas";
-import { formatedTodayDate } from "@/utils/date";
-import { AddRounded } from "@mui/icons-material";
-import { Box, Button, Card, Typography } from "@mui/material";
+import {
+    ArrowDownwardRounded,
+    ArrowDropDownRounded,
+} from "@mui/icons-material";
+import { Box, Button, Card, IconButton, Typography } from "@mui/material";
 
 const Dashboard = () => {
     return (
-        <div className="h-full">
+        <div className="h-full flex flex-col gap-8">
             <DashboardsPageHeader />
             <Box
                 sx={{
                     display: "flex",
                     flexWrap: "wrap",
                     gap: 2,
-                    mt: 4,
                 }}
             >
                 {dashboardCards.map((card) => (
@@ -43,11 +44,163 @@ const Dashboard = () => {
                 ))}
             </Box>
 
-            <div className="grid grid-cols-6 grid-rows-2 gap-4 h-full mt-8">
-                <Card className=" row-span-1 col-span-4 bg-gray-300! rounded-lg "></Card>
-                <Card className=" row-span-1 col-span-2 bg-gray-300! rounded-lg "></Card>
-                <Card className=" row-span-1 col-span-4 bg-gray-300! rounded-lg "></Card>
-                <Card className=" row-span-1 col-span-2 bg-gray-300! rounded-lg "></Card>
+            <div className="grid grid-cols-6 grid-rows-2 gap-4 h-full">
+                <Card className=" row-span-1 col-span-4 bg-gray-100! rounded-lg ">
+                    <Box className="border-b border-gray-300 p-2">
+                        <Typography variant="body2">
+                            نمودار دریافتی ها سی روز
+                        </Typography>
+                    </Box>
+                </Card>
+                <Card className=" row-span-1 col-span-2 bg-gray-100! rounded-lg ">
+                    <Box className="border-b border-gray-300 p-2">
+                        <Typography variant="body2">
+                            نمودار دریافتی ها سی روز
+                        </Typography>
+                    </Box>
+                </Card>
+                <Card className=" row-span-1 col-span-4 bg-gray-100! rounded-lg ">
+                    <Box className="border-b border-gray-300 p-2">
+                        <Typography variant="body2">تراکنش های اخیر</Typography>
+                    </Box>
+                    <Box className="flex flex-col px-4">
+                        <Box className="flex items-center justify-between py-2 border-b border-gray-300">
+                            <Box className="flex items-center gap-2">
+                                <IconButton color="success">
+                                    <ArrowDownwardRounded
+                                        fontSize="small"
+                                        color="success"
+                                    />
+                                </IconButton>
+                                <span className="flex flex-col">
+                                    <Typography variant="body2">
+                                        علی رضایی
+                                    </Typography>
+                                    <Typography variant="caption">
+                                        پرداخت شناسه c00134
+                                    </Typography>
+                                </span>
+                            </Box>
+                            <Box className="flex flex-col items-end">
+                                <Typography variant="body1" color="success" >
+                                    12000000 ريال
+                                </Typography>
+                                <Typography variant="caption">
+                                    2 ساعت پیش
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box className="flex items-center justify-between py-2 border-b border-gray-300">
+                            <Box className="flex items-center gap-2">
+                                <IconButton color="success">
+                                    <ArrowDownwardRounded
+                                        fontSize="small"
+                                        color="success"
+                                    />
+                                </IconButton>
+                                <span className="flex flex-col">
+                                    <Typography variant="body2">
+                                        علی رضایی
+                                    </Typography>
+                                    <Typography variant="caption">
+                                        پرداخت شناسه c00134
+                                    </Typography>
+                                </span>
+                            </Box>
+                            <Box className="flex flex-col items-end">
+                                <Typography variant="body1" color="success" >
+                                    12000000 ريال
+                                </Typography>
+                                <Typography variant="caption">
+                                    2 ساعت پیش
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box className="flex items-center justify-between py-2 border-b border-gray-300">
+                            <Box className="flex items-center gap-2">
+                                <IconButton color="success">
+                                    <ArrowDownwardRounded
+                                        fontSize="small"
+                                        color="success"
+                                    />
+                                </IconButton>
+                                <span className="flex flex-col">
+                                    <Typography variant="body2">
+                                        علی رضایی
+                                    </Typography>
+                                    <Typography variant="caption">
+                                        پرداخت شناسه c00134
+                                    </Typography>
+                                </span>
+                            </Box>
+                            <Box className="flex flex-col items-end">
+                                <Typography variant="body1" color="success" >
+                                    12000000 ريال
+                                </Typography>
+                                <Typography variant="caption">
+                                    2 ساعت پیش
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box className="flex items-center justify-between py-2 border-b border-gray-300">
+                            <Box className="flex items-center gap-2">
+                                <IconButton color="success">
+                                    <ArrowDownwardRounded
+                                        fontSize="small"
+                                        color="success"
+                                    />
+                                </IconButton>
+                                <span className="flex flex-col">
+                                    <Typography variant="body2">
+                                        علی رضایی
+                                    </Typography>
+                                    <Typography variant="caption">
+                                        پرداخت شناسه c00134
+                                    </Typography>
+                                </span>
+                            </Box>
+                            <Box className="flex flex-col items-end">
+                                <Typography variant="body1" color="success" >
+                                    12000000 ريال
+                                </Typography>
+                                <Typography variant="caption">
+                                    2 ساعت پیش
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box className="flex items-center justify-between py-2 border-b border-gray-300">
+                            <Box className="flex items-center gap-2">
+                                <IconButton color="success">
+                                    <ArrowDownwardRounded
+                                        fontSize="small"
+                                        color="success"
+                                    />
+                                </IconButton>
+                                <span className="flex flex-col">
+                                    <Typography variant="body2">
+                                        علی رضایی
+                                    </Typography>
+                                    <Typography variant="caption">
+                                        پرداخت شناسه c00134
+                                    </Typography>
+                                </span>
+                            </Box>
+                            <Box className="flex flex-col items-end">
+                                <Typography variant="body1" color="success" >
+                                    12000000 ريال
+                                </Typography>
+                                <Typography variant="caption">
+                                    2 ساعت پیش
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Card>
+                <Card className=" row-span-1 col-span-2 bg-gray-100! rounded-lg ">
+                    <Box className="border-b border-gray-300 p-2">
+                        <Typography variant="body2">نسیه های اخیر</Typography>
+                    </Box>
+                </Card>
             </div>
         </div>
     );
