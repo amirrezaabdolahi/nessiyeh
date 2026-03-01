e13f19da5183ef105986c1143da745563372c7f3
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-03-01 10:25:09 +0330

    started the customers page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -168,32 +168,6 @@ const Dashboard = () => {
                                 </Typography>
                             </Box>
                         </Box>
-                        <Box className="flex items-center justify-between py-2 border-b border-gray-300">
-                            <Box className="flex items-center gap-2">
-                                <IconButton color="success">
-                                    <ArrowDownwardRounded
-                                        fontSize="small"
-                                        color="success"
-                                    />
-                                </IconButton>
-                                <span className="flex flex-col">
-                                    <Typography variant="body2">
-                                        علی رضایی
-                                    </Typography>
-                                    <Typography variant="caption">
-                                        پرداخت شناسه c00134
-                                    </Typography>
-                                </span>
-                            </Box>
-                            <Box className="flex flex-col items-end">
-                                <Typography variant="body1" color="success" >
-                                    12000000 ريال
-                                </Typography>
-                                <Typography variant="caption">
-                                    2 ساعت پیش
-                                </Typography>
-                            </Box>
-                        </Box>
                     </Box>
                 </Card>
                 <Card className=" row-span-1 col-span-2 bg-gray-100! rounded-lg ">
