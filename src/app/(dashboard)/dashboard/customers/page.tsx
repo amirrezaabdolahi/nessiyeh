ae65da1cf5294432534e8d0a6637ccc7588cc7ce
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-03-02 16:36:20 +0330

    created the customer page but without style

--- src/app/(dashboard)/dashboard/customers/page.tsx
+++ src/app/(dashboard)/dashboard/customers/page.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import DashboardsPageHeader from "@/components/dash/DashboardsPageHeader";
+import { Customers, CustomersBranchName } from "@/data/DashboardCustomers";
+import { AddRounded } from "@mui/icons-material";
+import { Box, Button, Card, Typography } from "@mui/material";
 
 const DashboardCustomers = () => {
-  return (
-    <div>DashboardCustomers</div>
-  )
-}
+    return (
+        <div className="h-full flex flex-col gap-8">
+            <DashboardsPageHeader
+                title="مشتری ها"
+                caption="247 مشتری ثبت نام کرده ، 6 نا مشتری این ماه"
+            >
+                <Button endIcon={<AddRounded />} variant="contained">
+                    مشتری
+                </Button>
+            </DashboardsPageHeader>
+            <Box className="w-full overflow-x-scroll xl:overflow-auto">
+                <Card className="w-300 xl:w-full rounded-lg! sticky top-0 z-50 grid grid-cols-8 items-center justify-between">
+                    {CustomersBranchName.map((name, index) => (
+                        <Typography key={index} variant="body2" className="text-start">
+                            {name}
+                        </Typography>
+                    ))}
+                </Card>
+                {Customers.map((customer) => (
+                    <Box
+                        key={customer.id}
+                        className="w-300 xl:w-full rounded-lg! sticky top-0 z-50 grid grid-cols-8 items-center justify-between"
+                    >
+                        <Typography variant="body2" className="text-start">
+                            {customer.username}
+                        </Typography>
+                        <Typography variant="body2" className="text-start">
+                            {customer.phone}
+                        </Typography>
+                        <Typography variant="body2" className="text-start">
+                            {customer.totalCredit} ریال
+                        </Typography>
+                        <Typography variant="body2" className="text-start">
+                            {customer.paid} ریال
+                        </Typography>
+                        <Typography variant="body2" className="text-start">
+                            {customer.totalCredit - customer.paid} ریال
+                        </Typography>
+                        <Typography variant="body2" className="text-start">
+                            {customer.status}
+                        </Typography>
+                        <Typography variant="body2" className="text-start">
+                            {customer.lastPayment}
+                        </Typography>
+                        <Typography variant="body2" className="text-start">
+                            {customer.username}
+                        </Typography>
+                    </Box>
+                ))}
+            </Box>
+        </div>
+    );
+};
 
-export default DashboardCustomers
+export default DashboardCustomers;
---
+++ src/data/DashboardCustomers.ts
@@ -0,0 +1,73 @@
+
+
+type Customer = {
+    id: number,
+    username: string,
+    phone: string,
+    totalCredit: number,
+    paid: number,
+    status: "active" | "overdue" | "settled",
+    lastPayment: string
+}
+
+
+export const Customers: Array<Customer> = [
+    {
+        id: 1,
+        username: "علیرضا",
+        phone: "09225621256",
+        totalCredit: 30_000_000,
+        paid: 12_000_000,
+        status: "active",
+        lastPayment: "12 اردیبهشت"
+    },
+    {
+        id: 2,
+        username: "مریم حسینی",
+        phone: "09225621256",
+        totalCredit: 10_000_000,
+        paid: 5_000_000,
+        status: "overdue",
+        lastPayment: "13 اردیبهشت"
+    },
+    {
+        id: 3,
+        username: "رضا کریمی",
+        phone: "09225621256",
+        totalCredit: 6_000_000,
+        paid: 0,
+        status: "active",
+        lastPayment: "16 اردیبهشت"
+    },
+    {
+        id: 4,
+        username: "فاطمه احمدی",
+        phone: "09225621256",
+        totalCredit: 60_000_000,
+        paid: 59_000_000,
+        status: "active",
+        lastPayment: "12 اردیبهشت"
+    },
+    {
+        id: 5,
+        username: "حسن مرادی",
+        phone: "09225621256",
+        totalCredit: 2_000_000,
+        paid: 400_000,
+        status: "active",
+        lastPayment: "12 اردیبهشت"
+    },
+    {
+        id: 6,
+        username: "کامران تهرانی",
+        phone: "09225621256",
+        totalCredit: 11_000_000,
+        paid: 3_000_000,
+        status: "active",
+        lastPayment: "12 اردیبهشت"
+    },
+]
+
+export const CustomersBranchName : Array<string> = [
+    "مشتری","شماره","جمع حساب","پرداخت شده","باقیمانده","شرایط","آخرین پرداخت","",
+]
