dfbb514d77a752a094814564831093747e5d48f4
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-03-07 17:41:05 +0330

    some changes its war what you espect fu***

--- src/app/(dashboard)/dashboard/orders/page.tsx
+++ src/app/(dashboard)/dashboard/orders/page.tsx
@@ -1,10 +1,21 @@
+import DashboardsPageHeader from "@/components/dash/DashboardsPageHeader";
 import { CreditBranchName } from "@/data/DashboardCredits";
 import BranchHead from "@/features/dashboard/components/BranchHead";
+import { AddRounded } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import React from "react";
 
 const Orders = () => {
     return (
-        <div>
+        <div className="h-full flex flex-col gap-8 ">
+            <DashboardsPageHeader
+                title="حساب ها"
+                caption="128 حساب ، 180000000 ریال جمع حساب ها"
+            >
+                <Button endIcon={<AddRounded />} variant="contained">
+                    حساب
+                </Button>
+            </DashboardsPageHeader>
             <BranchHead branches={CreditBranchName} />
         </div>
     );
