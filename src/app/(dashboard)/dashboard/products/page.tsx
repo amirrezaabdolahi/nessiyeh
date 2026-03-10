6f22a98d5a7c4f8f12e1a68ee3187ae190e64b2e
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-03-10 12:43:51 +0330

    some changes to product ui

--- src/app/(dashboard)/dashboard/products/page.tsx
+++ src/app/(dashboard)/dashboard/products/page.tsx
@@ -2,6 +2,7 @@ import BranchSelect from "@/components/dash/BranchSelectField";
 import CategorySelect from "@/components/dash/CategorySelectField";
 import DashboardsPageHeader from "@/components/dash/DashboardsPageHeader";
 import { productsShow } from "@/data/DashboardProducts";
+import Product from "@/features/dashboard/components/Product";
 import { AddRounded, CameraRounded, ImageRounded } from "@mui/icons-material";
 import {
     Autocomplete,
@@ -36,43 +37,9 @@ const Products = () => {
                 </div>
             </div>
             <Box>
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 gap-4">
                     {productsShow.map((product) => (
-                        <Link
-                            key={product.id}
-                            href={`products/${product.id}`}
-                            className="h-full"
-                        >
-                            <Card className="rounded-lg! h-full">
-                                <div className="w-full h-80 flex items-center justify-center">
-                                    <ImageRounded color="action" />
-                                </div>
-                                <div className="p-2">
-                                    <Typography variant="body1">
-                                        {product.name}
-                                    </Typography>
-                                    {product.sku && (
-                                        <Typography variant="body2">
-                                            بارکد : {product.sku}
-                                        </Typography>
-                                    )}
-                                    <Typography variant="body2">
-                                        قیمت : {product.sell_price}
-                                    </Typography>
-                                    {product.exp_date && (
-                                        <Typography variant="body2">
-                                            تاریخ انقضا : {product.exp_date}
-                                        </Typography>
-                                    )}
-                                    <Typography variant="body2">
-                                        {product.category}
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        {product.unit} : {product.qty}
-                                    </Typography>
-                                </div>
-                            </Card>
-                        </Link>
+                        <Product key={product.id} product={product} />
                     ))}
                 </div>
             </Box>
