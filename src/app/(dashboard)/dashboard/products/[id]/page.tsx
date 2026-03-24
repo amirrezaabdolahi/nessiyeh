e0f66a239853b02a0f7d81f825e0545c24357e58
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-03-24 18:43:16 +0330

    created the product page , start to creat the edit product page

--- src/app/(dashboard)/dashboard/products/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/products/[id]/page.tsx
@@ -2,13 +2,16 @@ import Container from "@/components/dash/Container";
 import CopyButtonSku from "@/features/dashboard/components/CopyButtonSku";
 import ProductPageHeader from "@/features/dashboard/components/ProductPageHeader";
 import GetProduct from "@/features/dashboard/products/api/GetProduct";
-import { Button, Card, Typography } from "@mui/material";
+import { Avatar, Button, Card, Typography } from "@mui/material";
 
 const Product = async ({ params }: { params: { id: string } }) => {
     const { id } = await params;
 
     const product = await GetProduct({ id: id });
-
+    const formatter = new Intl.NumberFormat("fa-IR", {
+        style: "currency",
+        currency: "IRR",
+    });
     console.log(product);
 
     if (!product) {
@@ -24,20 +27,48 @@ const Product = async ({ params }: { params: { id: string } }) => {
     return (
         <Container>
             <ProductPageHeader id={id} />
-            <Card className="p-4">
-                <div className="flex items-center gap-4">
-                    <Typography variant="h6">{product.name}</Typography>
-                    {product.sku && <CopyButtonSku text={product.sku} />}
-                </div>
-                <Typography variant="caption">{product.description}</Typography>
-                <div className="">
-                    <Typography variant="body1">
-                        قیمت خرید : {product.buy_price}
+            <Card className="p-4 flex items-center justify-between">
+                <div className="flex flex-col gap-2">
+                    <div className="flex items-center gap-4">
+                        <Typography variant="h6">{product.name}</Typography>
+                        {product.sku && <CopyButtonSku text={product.sku} />}
+                    </div>
+                    <Typography variant="caption">
+                        {product.description}
                     </Typography>
-                    <Typography variant="body1">
-                        قیمت فروش : {product.sell_price}
-                    </Typography>
+                    <div className="">
+                        <Typography variant="h6">
+                            قیمت خرید : {formatter.format(product.buy_price)}
+                        </Typography>
+                        <Typography variant="h6">
+                            قیمت فروش : {formatter.format(product.sell_price)}
+                        </Typography>
+                    </div>
+                    <div className="">
+                        <Typography variant="body1">
+                            {product.man_date} - {product.exp_date}
+                        </Typography>
+                    </div>
+                    <div className="">
+                        <Typography variant="body1">
+                            دسته بندی : {product.category}
+                        </Typography>
+                        <Typography variant="body1">
+                            شاخه : {product.branch}
+                        </Typography>
+                        <Typography variant="body1">
+                            نوع شمارش : {product.unit}
+                        </Typography>
+                    </div>
+                    <div className="">
+                        <Typography variant="h6">
+                            موجودی : {product.qty}
+                        </Typography>
+                    </div>
                 </div>
+                <Avatar variant="rounded" className="w-70! h-90!" alt={product.name} >
+                    {product.name[0]}
+                </Avatar>
             </Card>
         </Container>
     );
